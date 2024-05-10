80772cd8d40675d0d916073a6ea12a954d0d6643
George2G <georgigoranov03@gmail.com>
2024-05-10 10:59:47 +0200

    projects slide image

--- js/script.js
+++ js/script.js
@@ -73,3 +73,24 @@ overlay.addEventListener('click', () => {
         closeModal(modal)
     })
 })
+
+
+const scrollContainer = document.querySelector(".gallery");
+const backBtn = document.getElementById("backBtn")
+const nextBtn = document.getElementById("nextBtn")
+
+scrollContainer.addEventListener("wheel",(evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = "auto";
+})
+
+nextBtn.addEventListener("click", ()=>{
+    scrollContainer.style.scrollBehavior ="smooth";
+    scrollContainer.scrollLeft +=450;
+})
+
+backBtn.addEventListener("click", ()=>{
+    scrollContainer.style.scrollBehavior ="smooth";
+    scrollContainer.scrollLeft -=450;
+})
